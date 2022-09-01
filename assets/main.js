12d9e97b5b8afe7770e2af109cec5bfff54b4f7b
taukirshopify <taukirshopify@gmail.com>
2022-09-01 22:45:58 +0600

    update js newlatter

--- assets/main.js
+++ assets/main.js
@@ -88,7 +88,7 @@ function windowOnClick(event) {
     }
 }
 
-trigger.addEventListener("click", toggleModal);
+
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
