a122e7754508dae6688fc0e04d3fc3857586ffd9
taukirshopify <taukirshopify@gmail.com>
2022-09-02 15:00:09 +0600

    all collection margin update

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,4 @@
-/*---------------------------------------------
-Template name:  taukir-quickstart || html template
-Version:        1.0
-Author:  MD Taukir CH
-Author url:     https://github.com/rajibmehedihasan
 
-NOTE:
-------
-Please DO NOT EDIT THIS JS, you may need to use "custom.js" file for writing your custom js.
-We may release future updates so it will overwrite this file. it's better and safer to use "custom.js".
-
-[Table of Content]
-----------------------------------------------*/
 
 (function ($) {
 
